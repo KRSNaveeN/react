a0e83523348d46145f3f8468a1656bc432568c6b
KRSNaveeN <117568376+KRSNaveeN@users.noreply.github.com>
2023-12-09 19:41:59 +0530

    Add files via upload

--- Expenseitem.js
+++ Expenseitem.js
@@ -2,20 +2,23 @@ import ExpenseDate from "./ExpenseDate";
 import "./Expenseitem.css";
 import { useState } from "react";
 const Expenseitem = (props)=> {
-  const[value,setValue]=useState(true);
+  const[title,setTitle]=useState(props.title);
+  const [amount, setAmount] = useState(props.amount)
   function clickHandler(){
-        setValue(false);
+        setTitle("Updated Title");
+        setAmount("$100");
   }
   return (
     <>
-   {value && (<div className="expense-item">
+    <div className="expense-item">
       <ExpenseDate data={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-        <button onClick={clickHandler}>delete</button>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+        {/* <button onClick={clickHandler}>change title</button> */}
+        <button onClick={clickHandler}>change title</button>
       </div>
-    </div>)}
+    </div>
     </>
   );
 }
