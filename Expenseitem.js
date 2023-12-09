6e41dd3217af6b00f606ec0a50220a5bc5f61c55
KRSNaveeN <117568376+KRSNaveeN@users.noreply.github.com>
2023-12-09 17:12:18 +0530

    Add files via upload

--- Expenseitem.js
+++ Expenseitem.js
@@ -1,15 +1,22 @@
 import ExpenseDate from "./ExpenseDate";
 import "./Expenseitem.css";
-
+import { useState } from "react";
 const Expenseitem = (props)=> {
+  const[value,setValue]=useState(true);
+  function clickHandler(){
+        setValue(false);
+  }
   return (
-    <div className="expense-item">
+    <>
+   {value && (<div className="expense-item">
       <ExpenseDate data={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>delete</button>
       </div>
-    </div>
+    </div>)}
+    </>
   );
 }
 
