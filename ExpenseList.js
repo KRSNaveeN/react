94b984fa78d00fd0d75ea1eae7ce34b9bf272911
KRSNaveeN <117568376+KRSNaveeN@users.noreply.github.com>
2023-12-10 07:55:51 +0530

    Add files via upload

--- ExpenseList.js
+++ ExpenseList.js
@@ -1,14 +1,22 @@
 import Expenseitem from './Expenseitem'
+import { useState } from 'react'
 import './ExpenseList.css'
 function ExpenseList(props){
+  let [value, setValue] = useState(false)
   if(props.arr.length === 0)
   {
     return <p className='expenses-list__fallback'>Content Missing</p>
   }
+  if(props.arr.length  == 1)
+  {
+    //  let  x = <h2>Add more items</h2>
+    value=true;
+    
+  }
    return(
     // let contentDat = <p>Missing content</p>
     // if(filteredExpenses.length > 0)
-    // if(props.arr.length >0)
+   <>
     <ul className='expense-list'>
     {
     // contentDat = filteredExpenses.map((items) => {
@@ -24,6 +32,8 @@ function ExpenseList(props){
                                    })
    }
    </ul>
+   {value && <h2>Only single Expense. Please add more items</h2> }
+   </>
    )
    
 }
